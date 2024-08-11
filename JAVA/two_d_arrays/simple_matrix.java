d0c3b5cb7ff3a3f23ebcc249580afada63f23b93
BHARGAVROYAL18 <158485304+BHARGAVROYAL18@users.noreply.github.com>
2024-08-11 09:56:47 +0530

    hh

---
+++ JAVA/two_d_arrays/simple_matrix.java
@@ -0,0 +1,24 @@
+package ApniCollege.two_d_arrays;
+
+import java.util.Scanner;
+
+public class simple_matrix {
+    public static void main(String[] args) {
+        int[][] arr=new int[2][2];
+        int ele;
+        Scanner in=new Scanner(System.in);
+        for (int i = 0; i < 2; i++) {
+           for (int j = 0; j < 2; j++) {
+             ele=in.nextInt();
+             arr[i][j]=ele;
+           }
+        }
+        in.close();
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+              System.out.print(arr[i][j]+" ");
+            }
+            System.out.println(" ");
+         }
+    }
+}
